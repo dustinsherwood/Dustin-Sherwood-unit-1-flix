d8e0ddc82a3459e1ac742d46145316a79110424e
dustinsherwood <dsherwoo@fau.edu>
2022-09-06 15:49:20 -0400

    Initial Commit

---
+++ unit 1 flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unit 1 flix
+//
+//  Created by Victoria Piotrowski on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ unit 1 flixUITests/unit_1_flixUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  unit_1_flixUITestsLaunchTests.swift
+//  unit 1 flixUITests
+//
+//  Created by Victoria Piotrowski on 9/6/22.
+//
+
+import XCTest
+
+class unit_1_flixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
